b99aefff8682582f084e18bbc5d67f42b6a5864c
Julien FAURE <julien.faure26@gmail.com>
2019-09-20 10:35:26 +0200

    copy

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use http\Client\Curl\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ArticleController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index','destroy','show');
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::all();
+
+        return view('article.index', [
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        return view('article.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $article = new Article;
+        $article->title = $request->input('title');
+        $article->content = $request->input('content');
+        $article->user_id = Auth::id();
+        /**
+         *
+         * associer ici le User
+         *
+         */
+        $article->save();
+
+        return redirect('article');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        return view('article.show', [
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
+        if ($article->user_id == Auth::id()){
+            return view('article.edit', [
+                'article' => $article
+            ]);
+        }
+
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        $article->title = $request->input('title');
+        $article->content = $request->input('content');
+        $article->save();
+
+        return redirect(route('article.show', ['article' => $article]));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
+    {
+        //
+    }
+
+
+}
